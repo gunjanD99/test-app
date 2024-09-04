b698378467304a3d312031dfa15fbe4782101b9c
gunjanD99 <gunjandhoke5@gmail.com>
2024-09-04 16:12:56 +0530

    first commit

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ function closeModal() {
     let modal = document.getElementById("myModal");
     modal.style.display = "none";
 }
+function openModal(imageSrc) {
+    let modal = document.getElementById("myModal");
+    let modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+}
+
+function closeModal() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
     
